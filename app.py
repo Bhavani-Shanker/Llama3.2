8c522726967400b66a7424cbcd109de30d2d552c
Bhavani-Shanker <33624045+Bhavani-Shanker@users.noreply.github.com>
2024-09-28 10:03:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,72 @@
+import requests
+import json
+import streamlit as st
+
+url = "http://localhost:11434/api/generate"
+
+
+headers = {
+    'Content-Type': 'application/json'
+}
+
+if "history" not in st.session_state:
+    st.session_state.history = []
+
+
+def generate_response(prompt):
+    st.session_state.history.append(prompt)
+    final_prompt = "\n".join(st.session_state.history)
+
+    data = {
+        "model": "llama3.2",
+        "prompt": final_prompt,
+        "stream": False
+    }
+
+    
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    
+    if response.status_code == 200:
+        data = response.json()
+        actual_response = data['response']
+        return actual_response
+    else:
+        st.error(f"Error: {response.text}")
+        return None
+
+
+st.set_page_config(page_title="LLaMA 3.2 Chat Interface", layout="wide")
+
+
+st.title("💬 LLama 3.2 Chat Interface")
+
+# Introduction
+st.markdown("""
+This application allows you to interact with the **LLaMA model**. 
+Simply enter your prompt below and click the button to receive a response.
+""")
+
+prompt = st.text_area("Enter your Prompt:", height=100)
+
+# Button to submit the prompt
+if st.button("Generate Response"):
+    if prompt:
+        
+        response = generate_response(prompt)
+        
+        
+        if response:
+            st.success("Response:")
+            st.markdown(f"> {response}")
+    else:
+        st.error("Please enter a prompt.")
+
+st.sidebar.title("Conversation History")
+if st.session_state.history:
+    for i, message in enumerate(st.session_state.history):
+        st.sidebar.markdown(f"{i + 1}. **User:** {message}")
+
+if st.sidebar.button("Clear History"):
+    st.session_state.history.clear()
+    st.sidebar.success("Conversation history cleared.")
